0bcba604f49636765baf101e5b8964900501e475
rtm516 <ryantmilner@hotmail.co.uk>
2021-03-07 22:48:53 +0000

    Improve signing progress

--- app/src/main/java/dev/projectearth/patcher/steps/ApkSign.java
+++ app/src/main/java/dev/projectearth/patcher/steps/ApkSign.java
@@ -5,6 +5,13 @@ import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import dev.projectearth.patcher.MainActivity;
 import dev.projectearth.patcher.R;
@@ -15,14 +22,31 @@ import kellinwood.security.zipsigner.optional.KeyStoreFileManager;
 import lombok.SneakyThrows;
 
 public class ApkSign extends LoggedRunnable {
+    private static final Map<String, Integer> progressEvents = new HashMap<>();
+
     @SneakyThrows
     @Override
     public void run() {
-        logEventListener.onLogLine(MainActivity.getAppContext().getResources().getString(R.string.step_sign_setup));
+        logEventListener.onLogLine("00% - " + MainActivity.getAppContext().getResources().getString(R.string.step_sign_setup));
         KeyStore keystore = KeyStoreFileManager.loadKeyStore(StorageLocations.getEarthKeystore().getPath(), "earth_test".toCharArray());
 
         // TODO: Setup a proper key
         ZipSigner zipSigner = new ZipSigner();
+
+        progressEvents.clear();
+        zipSigner.addProgressListener(event -> {
+            if (event.getPercentDone() % 5 == 0) {
+                String messageKey = Arrays.stream(event.getMessage().split(" ")).limit(3).collect(Collectors.joining(" "));
+                Integer prev = progressEvents.get(messageKey);
+                if (prev == null || prev != event.getPercentDone()) {
+                    progressEvents.put(messageKey, event.getPercentDone());
+
+                    logEventListener.onLogLine(String.format("%1$2s", event.getPercentDone()).replaceAll(" ", "0") + "% - " + event.getMessage() + "...");
+                }
+            }
+        });
+
+        zipSigner.issueLoadingCertAndKeysProgressEvent();
         Certificate cert = keystore.getCertificate("earth_test");
         X509Certificate publicKey = (X509Certificate)cert;
         Key key = keystore.getKey("earth_test", "earth_test".toCharArray());
@@ -30,7 +54,7 @@ public class ApkSign extends LoggedRunnable {
 
         zipSigner.setKeys( "earth_test", publicKey, privateKey, "SHA1withRSA", null);
 
-        logEventListener.onLogLine(MainActivity.getAppContext().getResources().getString(R.string.step_sign_signing));
+        logEventListener.onLogLine("00% - " + MainActivity.getAppContext().getResources().getString(R.string.step_sign_signing));
         zipSigner.signZip(StorageLocations.getOutFile().toString(), StorageLocations.getOutFileSigned().toString());
         logEventListener.onLogLine(MainActivity.getAppContext().getResources().getString(R.string.step_done));
     }
